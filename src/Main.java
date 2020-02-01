d63ff9b7176b11fcabb75a2fc2dc8eb21b683eb0
zafrir dotan <zafrir.dotan@gmail.com>
2020-02-01 12:54:13 +0200

    added Registration form to main

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		DroneSystem droneSystem1=new DroneSystem();
+		new RegistrationForm(droneSystem1);
 		droneSystem1.addClient("Tamir", "Tubul", "Ashdod", "Hakinor", 26, "0546777483", eSubscriptionType.SMALL_PACKAGE);
 		droneSystem1.addClient("Aviv", "Shtern", "Ramat Hasharon", "Sokolov", 65, "0526134630", eSubscriptionType.BIG_PACKAGE);
 		Client c= droneSystem1.clientList.get(0);

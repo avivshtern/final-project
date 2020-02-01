8e6f7bd2a659c453efa2c3d0c6280bcb57c7e1c4
zafrir dotan <zafrir.dotan@gmail.com>
2020-02-01 12:49:26 +0200

    11

--- src/UIMain.java
+++ src/UIMain.java
@@ -10,8 +10,6 @@ public class UIMain {
 		DroneSystem droneSystem = new DroneSystem();
 		
 		   new RegistrationForm(droneSystem);
-//		  Client client1 = new Client("avi", "Dotan", "ako","ha-ganim", 35, "0453344494", 123, eSubscriptionType.BIG_PACKAGE );
-//		   new DroneOrderForm(droneSystem, client1);
 
 	}
 
